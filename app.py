c378329ad8e3b886e5ade22d070c4b389ea4cdab
Ashif Raza <tumhara_github_email@gmail.com>
2026-06-29 23:04:33 +0530

    commit3

--- app.py
+++ app.py
@@ -150,5 +150,5 @@ def logout():
     session.pop('username', None)
     return redirect(url_for('login'))
 
-if __name__ == '__main__':
-    app.run("0.0.0.0", debug=True)
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=10000)
